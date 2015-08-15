69d2c236fde37b944047f374a136a91a4af0a018
maj009 <maj009@gmail.com>
2015-08-15 10:28:30 +0200

    Commit video 06
    Encolar archivos

---
+++ wp-content/themes/cursowordpress/functions.php
@@ -0,0 +1,27 @@
+<?php 
+
+// Añadir scripts
+if(!function_exists("unodepiera_scripts"))
+{
+	function unodepiera_scripts()
+	{
+		wp_enqueue_script( 'bootstrap-js', '//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js', array('jquery'), true);
+		wp_register_script( 'functions', get_template_directory_uri().'js/functions.js', array('jquery'));
+		wp_enqueue_script('functions' );
+
+	}
+	add_action( 'wp_enqueue_scripts', 'unodepiera_scripts');
+}
+
+// Registramos estilos
+if(!function_exists("unodepiera_styles"))
+{
+	function unodepiera_styles()
+	{
+		wp_enqueue_style( 'bootstrap', '//bootswatch.com/united/bootstrap.min.css' );
+        wp_register_style('styles', get_template_directory_uri() . '/css/styles.css');
+        wp_enqueue_style('styles');
+
+	}
+	add_action( 'wp_enqueue_scripts', 'unodepiera_styles');
+}
