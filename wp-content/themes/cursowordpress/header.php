0aaf0a9329f30279f99918d1738f28c8a6b2ec7d
maj009 <maj009@gmail.com>
2015-09-06 16:08:22 +0200

    Commit video 07
    Header

---
+++ wp-content/themes/cursowordpress/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+	
+<head>
+	<meta charset="<?php bloginfo('charset'); ?>">
+	<meta name='viewport' content='widt=device-width, initial-scale=1'>
+	<link rel="pingback" href="<?php bloginfo('pingback_url' ); ?>">
+	<?php wp_head(); ?>
+	<title>Document</title>
+</head>
+<body <?php body_class(); ?>>
+	<div class="container col-md-12">
