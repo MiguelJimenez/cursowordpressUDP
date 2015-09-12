c8fd76514eb5eb9955cf5fd97538912c91c70d93
maj009 <maj009@gmail.com>
2015-09-12 22:48:52 +0200

    commit video 26
    Personalizar buscador

--- wp-content/themes/cursowordpress/functions.php
+++ wp-content/themes/cursowordpress/functions.php
@@ -18,7 +18,7 @@ if (!function_exists('unodepiera_setup'))
 			'comment-list',
 			'gallery',
 			'caption'
-		);
+			);
 		add_theme_support('html5', $args );
 	}
 	add_action( 'after_setup_theme', 'unodepiera_setup');
@@ -43,8 +43,8 @@ if(!function_exists("unodepiera_styles"))
 	function unodepiera_styles()
 	{
 		wp_enqueue_style( 'bootstrap', '//bootswatch.com/united/bootstrap.min.css' );
-        wp_register_style('styles', get_template_directory_uri() . '/css/styles.css');
-        wp_enqueue_style('styles');
+		wp_register_style('styles', get_template_directory_uri() . '/css/styles.css');
+		wp_enqueue_style('styles');
 
 	}
 	add_action( 'wp_enqueue_scripts', 'unodepiera_styles');
@@ -56,7 +56,7 @@ require_once('navwalker/wp_bootstrap_navwalker.php');
 // Registrar la navegación
 register_nav_menus(array(
 	'primary'	=> __( 'Primary Menu', 'unodepiera' )
-) );
+	) );
 
 // Registro de Widgets 
 if (!function_exists('unodepiera_add_widgets_footer'))
@@ -76,10 +76,10 @@ if (!function_exists('unodepiera_add_widgets_footer'))
 				'after_widget'  => '</div>',
 				'before_title'  => '<h2 class="widgettitle">',
 				'after_title'   => '</h2>'
-			);
-		
+				);
+
 			register_sidebar( $args );
-		
+
 			register_sidebar( array(
 				'name'          => __( 'Footer Center', 'unodepiera' ),
 				'id'            => 'sidebar-footer-center',
@@ -89,7 +89,7 @@ if (!function_exists('unodepiera_add_widgets_footer'))
 				'after_widget'  => '</div>',
 				'before_title'  => '<h2 class="widgettitle">',
 				'after_title'   => '</h2>'
-			)); 
+				)); 
 
 			register_sidebar( array(
 				'name'          => __( 'Footer Right', 'unodepiera' ),
@@ -100,17 +100,17 @@ if (!function_exists('unodepiera_add_widgets_footer'))
 				'after_widget'  => '</div>',
 				'before_title'  => '<h2 class="widgettitle">',
 				'after_title'   => '</h2>'
-			)); 
+				)); 
+		}
+		add_action( 'widgets_init', 'unodepiera_add_widgets_footer');
 	}
-	add_action( 'widgets_init', 'unodepiera_add_widgets_footer');
-}
 
 // Registro Sidebar
-if (!function_exists('unodepiera_add_sidebar'))
-{
-	function unodepiera_add_sidebar()
+	if (!function_exists('unodepiera_add_sidebar'))
 	{
-		register_sidebar( array(
+		function unodepiera_add_sidebar()
+		{
+			register_sidebar( array(
 				'name'          => __( 'Main Sidebar', 'unodepiera' ),
 				'id'            => 'sidebar-frontal',
 				'description'   => __( 'Sidebar para la página principal', 'unodepiera' ),
@@ -119,10 +119,29 @@ if (!function_exists('unodepiera_add_sidebar'))
 				'after_widget'  => '</div>',
 				'before_title'  => '<h2 class="widgettitle">',
 				'after_title'   => '</h2>'
-			)); 
+				)); 
 
+		}
+		add_action( 'widgets_init', 'unodepiera_add_sidebar');
 	}
-	add_action( 'widgets_init', 'unodepiera_add_sidebar');
-}
 
-require_once(trailingslashit( get_template_directory()).'includes/comments.php');
+	require_once(trailingslashit( get_template_directory()).'includes/comments.php');
+
+	if( ! function_exists( "unodepiera_search_form" ) )
+	{
+		function unodepiera_search_form( $form )
+		{
+			$form = '<form role="search" method="get" id="searchform" class="searchform" action="' . home_url( '/' ) . '" >
+			<div><label class="screen-reader-text" for="s">' . __( 'Buscar para', 'unodepiera' ) . '</label>
+			<div class="input-group">
+			<input type="text" class="form-control" value="' . get_search_query() . '" name="s" id="s" placeholder="Ingresa aquí tu búsqueda...">
+			<span class="input-group-btn">
+			<button class="btn btn-default" id="searchsubmit" type="submit">'. esc_attr__( 'Search' ) .'</button>
+			</span>
+			</div><!-- /input-group -->
+			</div>
+			</form>';
+			return $form;
+		}
+		add_filter( 'get_search_form', 'unodepiera_search_form' );
+	}
