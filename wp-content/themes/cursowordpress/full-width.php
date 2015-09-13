cba3c58e48d75250249ba53ffb4a4e29ac938def
maj009 <maj009@gmail.com>
2015-09-13 10:44:34 +0200

    commit video 32
    Full-width

---
+++ wp-content/themes/cursowordpress/full-width.php
@@ -0,0 +1,38 @@
+<?php 
+/*
+Template Name: Full Width Page
+ */
+?>
+<?php get_header(); ?>
+<div class="col-md-12 col-sm-12 col-xs-12">
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+	<!-- post -->
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h3><?php the_title(); ?></h3>
+		</div>
+		<div class="panel-body">
+			<?php 
+				if (has_post_thumbnail())
+				{
+					?>
+						<div class="col-md-6">
+							<?php the_post_thumbnail( 'medium', array('class' => 'img-responsive')); ?>
+						</div>
+					<?php 
+				}
+			 ?>
+			 <div class="col-md-12 col-sd-12 col-xs-12">
+			 	<?php the_content(); ?>
+			 </div>
+		</div>
+		
+	</div>
+	<?php endwhile; ?>
+	<!-- post navigation -->
+	<?php else: ?>
+	<!-- no posts found -->
+	<?php endif; ?>
+</div>
+
+<?php get_footer(); ?>
